3e6c5ae8ffd7451fa61348f283b82400bea0b68b
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-20 19:42:00 +0700

    word component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,40 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  txtEn = '';
-  txtVn = '';
-  mode = 1;
   words: Word[] = [
     { en: 'one', vn: 'mot', isMemorized: true },
     { en: 'two', vn: 'hai', isMemorized: false },
     { en: 'three', vn: 'ba', isMemorized: true }
   ];
-  styleMemorized = { color: 'green', fontWeight: 'normal' };
-  styleNotMemorized = { color: 'red', fontWeight: 'bold' };
-
-  getClasses(word: Word) {
-    if (word.isMemorized) return { daNho: true, chuaNho: false };
-    return { daNho: false, chuaNho: true };
-  }
-
-  addWord() {
-    const word: Word = { isMemorized: false, vn: this.txtVn, en: this.txtEn };
-    this.words.push(word);
-    this.txtEn = '';
-    this.txtVn = '';
-  }
-
-  removeWord(word: Word) {
-    const index = this.words.findIndex(w => w.en === word.en);
-    // const index = this.words.indexOf(word);
-    this.words.splice(index, 1);
-  }
-
-  shouldShowWord(word: Word) {
-    if (this.mode === 1) return true;
-    if (this.mode === 2 && word.isMemorized) return true;
-    return this.mode === 3 && !word.isMemorized;
-  }
 }
 
 interface Word {
